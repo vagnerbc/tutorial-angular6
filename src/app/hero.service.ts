6e6c486bd22164586ba6310b2e09b3fbe831e33b
Vagner Bernal de castro <vagner.bcastro@gmail.com>
2018-06-05 14:16:29 -0300

    Add routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,6 +9,10 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
